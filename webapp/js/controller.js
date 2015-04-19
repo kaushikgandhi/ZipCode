fcfdd666c911fb55f207cb4acb9ee60ddd71eeff
kaushik gandhi <kaushikfrnd@gmail.com>
2015-04-20 00:52:29 +0530

    Web App For showing Search results

---
+++ webapp/js/controller.js
@@ -0,0 +1,16 @@
+var myApp = angular.module('zipapp',[]);
+
+myApp.controller('zipController', ['$scope','$http', function($scope,$http) {
+  $scope.query = '';
+  $scope.table_data = '';
+      $scope.search_zipcode = function(){
+  	$http({url:'http://54.69.200.1/api/v1/search?query='+$scope.query,method:'GET'}).success(function(){
+
+  			$scope.table_data = data;
+  			
+  	}).error(function(){
+  		$scope.table_data = '';
+  	});
+  };
+
+}]);
